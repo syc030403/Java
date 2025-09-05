1cbfba067341afdda451c8aca06a1c1d26d7bb6c
syc030403 <syc030403@naver.com>
2025-09-05 14:03:35 +0900

    Edit Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+/*
 public class Main {
 	public static void main(String[]args) {
 		System.out.println("Hello World!");
+	}
+*/
+
+import java.util.Scanner;
+
+public class Main {
+	public static int Sum(int number1, int number2) {
+		return number1 + number2;
+	}
+	
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		
+		int inputNumber1 = scanner.nextInt();
+		int inputNumber2 = scanner.nextInt();
+		
+		
+		System.out.println(inputNumber1+inputNumber2);
+		return;
+				
 	}
 }

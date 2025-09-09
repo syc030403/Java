7db0ad4a25cfa632e3af9cc1fabe1df1b8e190e3
syc030403 <syc030403@naver.com>
2025-09-09 13:55:33 +0900

    Update BojNo. 2562

--- src/Main.java
+++ src/Main.java
@@ -278,6 +278,32 @@ public class Main {
 	}
 }
 */
+// BojNo. 2562
+
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int array[] = new int[9];
+		int max = 0;
+		int maxIndex = 0;
+		for(int i = 0; i < 9; i++) {
+			array[i] = scanner.nextInt();
+		}
+		
+		for(int i = 0; i < 9; i++) {
+			if(max <= array[i]) {
+				max = array[i];
+				maxIndex = i;
+			}
+		}
+		System.out.println(max);
+		System.out.println(maxIndex+1);
+	}
+}
+
 
 
 
